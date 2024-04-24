c0bcdefd08e6727fc1dfdb9696f41c77a16c6fa6
Charles <charlescretois@gmail.com>
2024-04-24 16:34:10 +0200

    feat: code fetch

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,6 +1,10 @@
 <script>
 import hljs from 'highlight.js'
 import CodeEditor from 'simple-code-editor'
+import python from 'highlight.js/lib/languages/python'
+
+hljs.registerLanguage('python', python)
+
 export default {
   components: {
     CodeEditor
--- src/composables/code.ts
+++ src/composables/code.ts
@@ -1,6 +1,6 @@
 export async function fetchData(language: string, code: string) {
   try {
-    const response = await fetch('https://34.74.112.135/execute', {
+    const response = await fetch('http://34.74.112.135/execute', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -13,7 +13,9 @@ export async function fetchData(language: string, code: string) {
     }
 
     const data = await response.json()
-    return data.result
+    if (data.error !== '') throw data.error
+    console.log(data)
+    return data.output
   } catch (error) {
     console.error('Error fetching data:', error)
     throw error
