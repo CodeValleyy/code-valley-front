b3458cf59e479accdeb6c6dfe270fce9bc4b6b8c
Ronan Kielt <kakapo9156@gmail.com>
2024-06-15 00:19:10 +0200

    feat: implement search view which calls the api

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,5 +1,6 @@
 import { ref } from 'vue';
 import axiosInstance from '@/config/axiosInstance';
+import type { UserResponse } from '@/types/UserResponse'
 import { useUserStore } from '@/stores/userStore';
 
 const token = ref(localStorage.getItem('token') ?? '');
@@ -7,7 +8,7 @@ const token = ref(localStorage.getItem('token') ?? '');
 const setToken = (newToken: string) => {
     token.value = newToken;
     localStorage.setItem('token', newToken);
-};
+}
 
 const resetToken = () => {
     token.value = '';
@@ -25,7 +26,7 @@ const fetchMe = async () => {
         console.error('Error fetching profile:', error);
         throw error;
     }
-};
+}
 
 const fetchProfile = async (userId: number | string | undefined) => {
     if (!userId) {
@@ -89,7 +90,17 @@ const uploadAvatar = async (file: File) => {
         console.error('Error uploading avatar:', error);
         throw error;
     }
-};
+}
+
+const searchUser = async (username: string): Promise<UserResponse[]> => {
+    try {
+        const response = await axiosInstance.get('/auth/search/' + username);
+        return response.data as UserResponse[];
+    } catch (error) {
+        console.error('Error fetching profile:', error);
+        throw error;
+    }
+}
 
 export const useAuth = () => ({
     setToken,
@@ -100,4 +111,5 @@ export const useAuth = () => ({
     fetchProfile,
     resetToken,
     uploadAvatar,
+    searchUser,
 });
---
+++ src/types/UserResponse.ts
@@ -0,0 +1,8 @@
+export interface UserResponse {
+  id: number;
+  email: string;
+  username: string;
+  createdAt: string;
+  lastLoginAt: string;
+  avatar: string;
+}
