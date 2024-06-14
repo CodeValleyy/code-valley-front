743511fd6b63f4683c5820ce55bf20305f180c3d
Ronan Kielt <kakapo9156@gmail.com>
2024-06-15 00:19:10 +0200

    feat: implement search view which calls the api

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,7 +1,50 @@
 <template>
   <v-container>
-    <h1>Recherche</h1>
+    <div>
+      <v-text-field
+        v-model="inputValue"
+        label="Rechercher un profil"
+        @input="onInputChange"
+        outlined
+        class="mb-4"
+      />
+      <ul>
+        <li v-for="profile in filteredProfiles" :key="profile.id">
+          <div class="mb-3">
+            <router-link :to="`/profile/${profile.id}`">
+              <v-avatar class="mr-3">
+                <img :src="getAvatar(profile)" alt="Avatar" />
+              </v-avatar>
+              {{ profile.username }}
+            </router-link>
+          </div>
+        </li>
+      </ul>
+    </div>
   </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAuth } from '@/composables/useAuth'
+import type { UserResponse } from '@/types/UserResponse'
+
+const { searchUser } = useAuth()
+
+let inputValue = ref<string>("")
+let filteredProfiles = ref<UserResponse[]>([])
+
+const onInputChange = async () => {
+  const searchQuery = inputValue.value.trim()
+  if (searchQuery !== "") {
+    filteredProfiles.value = await searchUser(searchQuery)
+  }
+}
+
+const getAvatar = (profile: UserResponse) => {
+  if (profile.avatar == null || !profile.avatar) {
+    return "https://image.noelshack.com/fichiers/2024/21/4/1716483099-image-2024-05-23-185151555.jpg"
+  }
+  return profile.avatar
+}
+</script>
